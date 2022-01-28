9c4bc5079e5239f27e43d8c137a60ce180ed504b
gafish <gafish@qq.com>
2022-01-28 15:25:42 +0800

    点击打开所选文件夹

--- src/components/Main/Head/DirName/index.tsx
+++ src/components/Main/Head/DirName/index.tsx
@@ -1,15 +1,24 @@
 import { FC } from 'react'
+import { shell } from '@tauri-apps/api'
 
 import inject from 'hoc/inject'
 
 interface IProps {
-  name?: string
+  path?: string
 }
 
-export const DirName: FC<IProps> = ({ name = '' }) => {
-  return <span className="text-xs pl-5 text-gray-400">{name}</span>
+export const DirName: FC<IProps> = ({ path = '' }) => {
+  return (
+    <span
+      className="tooltip tooltip-bottom text-xs pl-5 text-gray-400 cursor-pointer hover:text-blue-500"
+      data-tip="点击打开文件夹"
+      onClick={() => shell.open(path)}
+    >
+      {path}
+    </span>
+  )
 }
 
 export default inject(store => ({
-  name: store.selectedDir,
+  path: store.selectedDir,
 }))(DirName)
--- src/components/Main/Head/index.tsx
+++ src/components/Main/Head/index.tsx
@@ -4,7 +4,7 @@ import DirName from './DirName'
 
 export const Head = () => {
   return (
-    <div className="flex justify-between pb-2 border-b">
+    <div className="flex justify-between items-end pb-2 border-b">
       <div className="flex justify-between items-end">
         <BtnChooseDir size="sm" />
         <DirName />
