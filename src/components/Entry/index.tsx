5ba8eb3d138eae39e51c81f8c0aec530b07e88be
gafish <gafish@qq.com>
2022-02-14 23:31:29 +0800

    更新图标

--- src/components/Entry/index.tsx
+++ src/components/Entry/index.tsx
@@ -2,7 +2,7 @@ import BtnChooseDir from '../BtnChooseDir'
 
 const Entry = () => {
   return (
-    <div className="h-full flex justify-center items-center bg-purple-100">
+    <div className="h-full flex justify-center items-center bg-green-50">
       <BtnChooseDir size="lg" clickSource="entry" />
     </div>
   )
